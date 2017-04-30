b7198dca2f8cb87b8a9d755c25fb00e6d008383e
Giulia Cacioli <giulia.cacioli@gmail.com>
2017-04-30 11:36:42 +0200

    Added new dependencies in package.json + added babel and webpack configuration + added babel and webpack configuration in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,14 @@ module.exports = function (grunt) {
         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         tasks: ['compass:server', 'autoprefixer']
       },
+      babel: {
+        files: ['<%= yeoman.app %>/src/dataHandler/searchParser.service.js'],
+        tasks: ['babel']
+      },
+      webpack: {
+        files: ['<%= yeoman.app %>/dist/searchParser.service.js'],
+        tasks: ['webpack']
+      },
       // gruntfile: {
       //   files: ['Gruntfile.js']
       // },
@@ -215,6 +223,27 @@ module.exports = function (grunt) {
           debugInfo: true
         }
       }
+    },
+
+    // Compiles ES6 to ES5
+    babel: {
+       options: {
+             sourceMap: true,
+             presets: ['es2015']
+       },
+       dist: {
+          files: {
+             'app/dist/searchParser.service.js': 'app/src/dataHandler/searchParser.service.js'
+          }
+       }
+     },
+
+    // Module bundler
+    webpack: {
+      options: {
+        progress: true
+      },
+      app: require('./webpack.config')
     },
 
     // Renames files for browser caching purposes
@@ -443,6 +472,8 @@ module.exports = function (grunt) {
       'wiredep',
       'concurrent:server',
       'autoprefixer',
+      'babel',
+      'webpack',
       'connect:livereload',
       'watch'
     ]);
